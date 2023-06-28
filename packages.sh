a6c9f11d805036887b674acb6347d60093692b38
techlm77 <dominicyoungjackson@gmail.com>
2023-06-28 02:18:14 +0100

    Add files via upload

--- packages.sh
+++ packages.sh
@@ -91,5 +91,3 @@ export PKG_CURL=https://curl.se/download/curl-7.88.1.tar.xz
 export PKG_GIT=https://www.kernel.org/pub/software/scm/git/git-2.39.2.tar.xz
 export PKG_NANO=https://www.nano-editor.org/dist/v7/nano-7.2.tar.xz
 export PKG_NEOFETCH=https://github.com/dylanaraps/neofetch/archive/refs/tags/7.1.0.tar.gz
-export PKG_MAKECA=https://github.com/lfs-book/make-ca/releases/download/v1.12/make-ca-1.12.tar.xz
-export PKG_LIBTASN=https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.19.0.tar.gz
